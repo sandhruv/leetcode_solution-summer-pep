806f515ceea7c0ddaf1e050dc3ddb8e9dd5ba86f
sandhruv <153658082+sandhruv@users.noreply.github.com>
2026-07-27 10:41:36 +0530

    Time: 4 ms (3.39%), Space: 29.6 MB (5.3%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+
+        stack <int> st;
+        
+        for(char ch : s){
+            st.push(ch);
+        }
+
+        for(int i=0;i<s.size();i++){
+
+            s[i]=st.top();
+            st.pop();
+        }
+    }
+};
